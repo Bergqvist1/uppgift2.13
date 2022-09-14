48e8c95722d7f9f8089e4721637be9e9006b4879
Admin <Admin@TEB123013>
2022-09-14 11:35:20 +0200

    Add project files.

---
+++ uppgift2.13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Uppgift_2_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad har du för lön?");
+            string strLön = Console.ReadLine();
+            Console.WriteLine("Vad har han för lön?");
+            string strLön2 = Console.ReadLine();
+            Console.WriteLine("Vad har hon för lön?");
+            string strLön3 = Console.ReadLine();
+
+            int intLön = int.Parse(strLön);
+            int intLön2 = int.Parse(strLön2);
+            int intLön3 = int.Parse(strLön3);
+
+            int Summa = (intLön + intLön2 + intLön3) / 3 ;
+
+            Console.WriteLine("Medelvärdet på era löner är " + Summa + "kr");
+
+
+        }
+    }
+}
